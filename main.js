0aceec44e6a9b8626a82e8f866ace426b86a335b
ish-issane <160911735+ish-issane@users.noreply.github.com>
2024-11-16 20:48:49 -0500

    Fixed error throw for invalid data

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ function date_sorting_algorithm(a, b) {
     if (raw_data) {
         const parsed_data = parse_data(raw_data);
         console.log(parsed_data);
-        if (parsed_data == null){
+        if (parsed_data.length<1){
             throw new Error("Invalid data.");
             alert("Data was not valid. Try different dates.");
         }
